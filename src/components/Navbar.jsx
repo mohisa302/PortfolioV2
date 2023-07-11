047e9686bd8f7c8395c65665633d71b5b207b935
mohi <mohisadeghi302@gmail.com>
2023-07-11 21:19:22 +0330

    Add skills

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ function Navbar() {
       </ul>
 
       {/*social icons*/}
-      <div className="hidden md:flex lg:flex fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a href="/" className="flex justify-between items-center w-full text-gray-300">
