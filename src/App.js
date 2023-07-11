fa2aa07eb46bd5558c3f243292d987f0574957b7
mohi <mohisadeghi302@gmail.com>
2023-07-11 20:48:36 +0330

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
